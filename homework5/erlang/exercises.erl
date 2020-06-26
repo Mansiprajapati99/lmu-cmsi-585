4c14873b08cd5ce22341e959aeb04fe01108ab34
Mansiprajapati99 <60244762+Mansiprajapati99@users.noreply.github.com>
2020-06-25 17:03:52 -0700

    Update exercises.erl

--- homework5/erlang/exercises.erl
+++ homework5/erlang/exercises.erl
@@ -1,1 +1,20 @@
 
+-module(strangeserver).
+-export([start/0]).
+
+start() ->
+    receive
+        {multiply,A,B,Pid} ->
+            Pid ! A*B;
+
+        {length, S,Pid} ->
+            Pid ! length(S);
+
+        _ -> 
+            io:format("I don't understand~n",[])
+
+        after 3100 ->
+            io:format("I am bored~n",[])
+    end,
+start().
+ 
